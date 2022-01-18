58773b407808d36d64d657ffae6a46fbffc43aec
Alexandra Meteleva <aasolomeina@yandex.ru>
2022-01-18 20:28:41 +0300

    First exersise

--- Ex_1.py
+++ Ex_1.py
@@ -6,7 +6,7 @@ my_friends = {'name': 'Tom', 'age': 23, 'has_cat': True, 'name': 'Anna', 'age': 
 user_name = input('Как Вас зовут? ')
 user_age = int(input('А Ваш возраст? '))
 print(number_1)
-print(number_1 / float_number_1)
+print(number_1 // float_number_1)
 print(my_string)
 print(my_list[1])
 print(f'Hello {user_name}, you are {user_age} years old.')
