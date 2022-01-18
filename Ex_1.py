49922e34ac00c6e8df7da172cefadd6e4220b090
Alexandra Meteleva <aasolomeina@yandex.ru>
2022-01-18 20:17:49 +0300

    Задание 1

---
+++ Ex_1.py
@@ -0,0 +1,12 @@
+number_1 = 13
+float_number_1 = 3.14
+my_string = 'Hi Alex'
+my_list = ['Alex', 'Tom', 'Ron']
+my_friends = {'name': 'Tom', 'age': 23, 'has_cat': True, 'name': 'Anna', 'age': 35, 'has_cat': False, 'name': 'Ron', 'age': 16, 'has_cat': True}
+user_name = input('Как Вас зовут? ')
+user_age = int(input('А Ваш возраст? '))
+print(number_1)
+print(number_1 / float_number_1)
+print(my_string)
+print(my_list[1])
+print(f'Hello {user_name}, you are {user_age} years old.')
