b8b78519e216a4fb28cd137ed3db0d1d4baf29d4
Alexandra Meteleva <97023546+AlexMeteleva@users.noreply.github.com>
2022-01-19 20:16:41 +0300

    Delete Ex_2.2.py
    no relevant

--- Ex_2.2.py
+++
@@ -1,6 +0,0 @@
-user_seconds = int(input('Введите количество секунд: '))
-number_of_seconds = user_seconds % 60
-number_of_minutes = (user_seconds // 60) % 60
-number_of_hours = user_seconds // 3600
-result = f'{number_of_hours:02}:{number_of_minutes:02}:{number_of_seconds:02}'
-print(result)
